da46a024a197df4eb91227dfe4a3a1a3eaa69958
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-04-28 21:07:35 +0200

    Add buffered reader example to hyper_bunchmark.sh

--- hyper_benchmark.sh
+++ hyper_benchmark.sh
@@ -11,4 +11,8 @@ cargo build --release --examples
 # Verify that the output is equal between chemfiles and ours.
 time target/release/examples/compare $trajectory $decimals
 
-hyperfine --shell=none --warmup 2 "target/release/examples/reader $trajectory" "target/release/examples/xdrreader $trajectory" "target/release/examples/cfreader $trajectory"
+hyperfine --shell=none --warmup 2 \
+	"target/release/examples/reader    $trajectory" \
+	"target/release/examples/reader    $trajectory :: buffered" \
+	"target/release/examples/xdrreader $trajectory" \
+	"target/release/examples/cfreader  $trajectory"
