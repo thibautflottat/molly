c6e1dba334dee1750723e70ec2340d89ffb805b2
Marieke Westendorp <ma3ke.cyber@gmail.com>
2024-04-28 20:27:23 +0200

    Add a couple more buffered benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, ErrorKind};
+use std::io::{BufReader, ErrorKind, Seek};
 
 use bencher::{benchmark_group, benchmark_main, Bencher};
 use molly::{
@@ -11,12 +11,18 @@ benchmark_main!(reading, decoding);
 benchmark_group!(
     reading,
     read_frame,
+    read_frame_buffered,
     read_frames,
     read_frames_buffered,
     read_frames_few_atoms,
-    read_frames_few_atoms_buffered
+    read_frames_few_atoms_buffered,
 );
-benchmark_group!(decoding, read_compressed_positions);
+benchmark_group!(
+    decoding,
+    read_compressed_positions,
+    read_compressed_positions_from_file,
+    read_compressed_positions_from_file_buffered,
+);
 
 const PATH: &str = "tests/trajectories/adk_oplsaa.xtc";
 
@@ -24,6 +30,19 @@ fn read_frame(b: &mut Bencher) {
     let mut reader = XTCReader::open(PATH).unwrap();
     let mut frame = Frame::default();
     b.iter(|| match reader.read_frame(&mut frame) {
+        Ok(_) => {}
+        Err(err) if err.kind() == ErrorKind::UnexpectedEof => reader.home().unwrap(),
+        Err(err) => panic!("{err}"),
+    });
+}
+
+fn read_frame_buffered(b: &mut Bencher) {
+    let mut reader = XTCReader::open(PATH).unwrap();
+    let mut frame = Frame::default();
+    b.iter(|| match {
+        // Note that we inline the read_frame function here. But it should be the same.
+        reader.read_frame_with_selection_buffered(&mut frame, &AtomSelection::All)
+    } {
         Ok(_) => {}
         Err(err) if err.kind() == ErrorKind::UnexpectedEof => reader.home().unwrap(),
         Err(err) => panic!("{err}"),
@@ -84,6 +103,52 @@ fn read_compressed_positions(b: &mut Bencher) {
         let mut data = BufReader::new(position_bytes);
         reader::read_compressed_positions::<molly::buffer::UnBuffered, _>(
             &mut data,
+            &mut positions,
+            precision,
+            &mut scratch,
+            &AtomSelection::Until(natoms as u32),
+        )
+        .unwrap()
+    });
+}
+
+fn read_compressed_positions_from_file(b: &mut Bencher) {
+    let natoms = 125;
+    // A hand-tweaked test frame, derived from `delinyah_smaller.xtc`. Describes 125 positions.
+    let mut file = std::fs::File::open("tests/trajectories/delinyah_tiny.xtc").unwrap();
+    let start = 60; // We skip the header.
+    file.seek(std::io::SeekFrom::Start(start)).unwrap();
+
+    let mut positions = vec![0.0; natoms * 3];
+    let mut scratch = Vec::new();
+    let precision = 1000.0;
+    b.iter(|| {
+        file.seek(std::io::SeekFrom::Start(start)).unwrap();
+        reader::read_compressed_positions::<molly::buffer::Buffer, _>(
+            &mut file,
+            &mut positions,
+            precision,
+            &mut scratch,
+            &AtomSelection::Until(natoms as u32),
+        )
+        .unwrap()
+    });
+}
+
+fn read_compressed_positions_from_file_buffered(b: &mut Bencher) {
+    let natoms = 125;
+    // A hand-tweaked test frame, derived from `delinyah_smaller.xtc`. Describes 125 positions.
+    let mut file = std::fs::File::open("tests/trajectories/delinyah_tiny.xtc").unwrap();
+    let start = 60; // We skip the header.
+    file.seek(std::io::SeekFrom::Start(start)).unwrap();
+
+    let mut positions = vec![0.0; natoms * 3];
+    let mut scratch = Vec::new();
+    let precision = 1000.0;
+    b.iter(|| {
+        file.seek(std::io::SeekFrom::Start(start)).unwrap();
+        reader::read_compressed_positions::<molly::buffer::UnBuffered, _>(
+            &mut file,
             &mut positions,
             precision,
             &mut scratch,
