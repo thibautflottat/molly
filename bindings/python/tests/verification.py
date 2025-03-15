228287e2fdbffa9715dabc2820cf983543105974
Marieke Westendorp <ma3ke.cyber@gmail.com>
2025-03-15 20:37:27 +0100

    Improve python bindings tests/verification.py script

--- bindings/python/tests/verification.py
+++ bindings/python/tests/verification.py
@@ -1,11 +1,11 @@
 import time
+from sys import stdout
 
-import molly
 import MDAnalysis as MDA
+import molly
 import numpy as np
 
-# a = np.array([[1, 2], [3, 4]])
-# molly.process_array(a)
+END = "\r" if stdout.isatty() else "\n"
 
 
 def setup_readers(path):
@@ -34,7 +34,9 @@ def read_all(path):
         start = time.time()
         molly_positions = molly_reader.pop_frame().positions
         if i % 10 == 0:
-            print(f"     molly: {(time.time() - start) * 1000:6.03f} ms/frame")
+            print(
+                f"     molly: {(time.time() - start) * 1000:6.03f} ms/frame", end="\r"
+            )
 
         assert mda_positions.tolist() == molly_positions.tolist()
         mda_frames.append(mda_positions.copy())
@@ -43,76 +45,80 @@ def read_all(path):
     return mda_frames, molly_frames
 
 
-def read_frames_slice(path, slice):
+def test_read_frames(path, full_mda_frames, frame_selection, atom_selection) -> float:
     _, molly_reader = setup_readers(path)
-    mda_frames, _ = read_all(path)
-    mda_frames = mda_frames[slice]
-    print(f"{len(mda_frames) = }")
-    # molly_frames = molly_reader.read_frames(frame_selection=slice, atom_selection=None)
-    molly_frames = molly_reader.read_frames(frame_selection=slice, atom_selection=None)
-    help(molly_reader)
-
-    # |  read_into_array(
-    # |      self,
-    # |      /,
-    # |      coordinate_array,
-    # |      boxvec_array,
-    # |      time_array=None,
-    # |      frame_selection=None,
-    # |      atom_selection=None
-    # |  )
-    # |      Read all frames into the provided `numpy.ndarray`.
-    # |
-    # |      The `coordinate_array` must have a shape of `(nframes, natoms, 3)`.
-    # |
-    # |      The `boxvec_array` must have a shape of `(nframes, 3, 3)`.
-    # |
-    # |      Returns `True` if the reading operation was successful.
-    # |
-    # |      # Note
-    # |
-    # |      This function can perform the reads in a buffered manner, depending on the value of the
-    # |      `buffered` attribute.
+    mda_frames = full_mda_frames[frame_selection]
+    nframes = len(mda_frames)
+    natoms = len(mda_frames[0])
+    print(f"\t\t{nframes = }, {natoms = }")
+    start = time.time()
+    molly_frames = molly_reader.read_frames(
+        frame_selection=frame_selection, atom_selection=atom_selection
+    )
+    duration = time.time() - start
 
     for i, (mda_positions, molly_frame) in enumerate(zip(mda_frames, molly_frames)):
         molly_positions = molly_frame.positions
-        print(i)
+        print("\t\t", i, end="\r")
 
         assert (
             mda_positions.tolist() == molly_positions.tolist()
-        ), f"{mda_positions = }\n{molly_positions = }"
+        ), f"""mda_positions and molly_positions do not match
+{mda_positions = }
+{molly_positions = }"""
+
+    return duration
 
 
-def read_into_array(path, slice):
+def test_read_into_array(
+    path, full_mda_frames, frame_selection, atom_selection
+) -> float:
     _, molly_reader = setup_readers(path)
-    mda_frames, _ = read_all(path)
-    mda_frames = mda_frames[slice]
+    mda_frames = full_mda_frames[frame_selection]
     nframes = len(mda_frames)
     natoms = len(mda_frames[0])
-    print(f"{nframes = }, {natoms = }")
+    print(f"\t\t{nframes = }, {natoms = }")
     molly_frames = np.zeros((nframes, natoms, 3), dtype=np.float32)
     molly_boxvecs = np.zeros((nframes, 3, 3), dtype=np.float32)
+    start = time.time()
     molly_reader.read_into_array(
-        molly_frames, molly_boxvecs, frame_selection=slice, atom_selection=None
+        molly_frames,
+        molly_boxvecs,
+        frame_selection=frame_selection,
+        atom_selection=atom_selection,
     )
+    duration = time.time() - start
 
     for i, (mda_positions, molly_positions) in enumerate(zip(mda_frames, molly_frames)):
-        print(i)
+        print("\t\t", i, end="\r")
 
         assert (
             mda_positions.tolist() == molly_positions.tolist()
         ), f"{mda_positions = }\n{molly_positions = }"
-    print("tada!!")
+
+    return duration
+
+
+def read_test(path, full_mda_frames, frame_selection=None, atom_selection=None):
+    print(f"TEST: {frame_selection = }, {atom_selection = }, {path = }")
+    print(" -\tread_frames")
+    dur = test_read_frames(path, full_mda_frames, frame_selection, atom_selection)
+    print(f"\tOK!\t\tReading took {dur:8.3} s.")
+    print(" -\tread_into_array")
+    dur = test_read_into_array(path, full_mda_frames, frame_selection, atom_selection)
+    print(f"\tOK!\t\tReading took {dur:8.3} s.")
 
 
 path = "../../tests/trajectories/trajectory_smol.xtc"
-read_into_array(path, slice(None, None))
-read_frames_slice(path, slice(None, None))
-read_frames_slice(path, slice(None, 20))
-read_frames_slice(path, slice(25, 50))
-read_frames_slice(path, slice(None, None, 2))
-read_frames_slice(path, slice(None, 20, 2))
-read_frames_slice(path, slice(25, 50, 2))
-read_frames_slice(path, slice(None, None, 3))
-read_frames_slice(path, slice(None, 20, 3))
-read_frames_slice(path, slice(25, 50, 3))
+full_mda_frames, _ = read_all(path)
+
+# Frame slices.
+read_test(path, full_mda_frames, slice(None, None))
+read_test(path, full_mda_frames, slice(None, 20))
+read_test(path, full_mda_frames, slice(25, 50))
+read_test(path, full_mda_frames, slice(None, None, 2))
+read_test(path, full_mda_frames, slice(None, 20, 2))
+read_test(path, full_mda_frames, slice(25, 50, 2))
+read_test(path, full_mda_frames, slice(None, None, 3))
+read_test(path, full_mda_frames, slice(None, 20, 3))
+read_test(path, full_mda_frames, slice(25, 50, 3))
